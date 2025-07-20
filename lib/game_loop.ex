581cde9da5536ab1df798b884fbe9ebf9c5abec9
Estia <83793315+Pedro-Ba@users.noreply.github.com>
2025-07-19 23:47:30 -0300

    small refactor for readability

--- lib/game_loop.ex
+++ lib/game_loop.ex
@@ -1,12 +1,12 @@
 defmodule GameLoop do
-  @moduledoc """
+@moduledoc """
   Documentation for `GameLoop`.
   """
 
-  @doc """
+@doc """
   Hello world.
 
-  ## Examples
+## Examples
 
   iex> WordleThing.hello()
   :world
@@ -21,7 +21,7 @@ defmodule GameLoop do
     top_five_map = Enum.map(WordleThing.top_N_letters(alphabet_sorted, 5), fn {k, _v} -> k end);
     IO.puts("Top five map below");
     IO.inspect(top_five_map);
-
+  
     possible_guesses = WordleThing.valid_words_from_top_letters(total_word_list, top_five_map);
     IO.inspect("Possible guesses below");
     IO.inspect(possible_guesses);
@@ -34,24 +34,16 @@ defmodule GameLoop do
 
     feedback = IO.gets("Enter colors (GYB): ") |> String.trim();
     if feedback == "GGGGG" do
-      IO.puts("Guessed the word; game over")
+        IO.puts("Guessed the word; game over")
     else 
-      new_alphabet_map = prune_alphabet(elem(best_guess, 0), feedback, alphabet_map);
-      new_possible_word_list = prune_possible_word_list(elem(best_guess, 0), feedback, possible_word_list);
-      gameloop(total_word_list, new_possible_word_list, new_alphabet_map);
+        new_alphabet_map = prune_alphabet(elem(best_guess, 0), feedback, alphabet_map);
+        new_possible_word_list = prune_possible_word_list(elem(best_guess, 0), feedback, possible_word_list);
+        gameloop(total_word_list, new_possible_word_list, new_alphabet_map);
     end
   end
 
   def prune_alphabet(best_guess, feedback, alphabet_map) do
-    black_positions = String.graphemes(feedback) |> Enum.with_index(0) |> Enum.filter(fn {k, v} -> 
-      if k == "B" do
-        v
-      end
-    end) 
-    letters_best_guess = String.graphemes(best_guess);
-    letters_to_remove = Enum.map(black_positions, fn {_k, v} ->
-      Enum.at(letters_best_guess, v)
-    end)
+    letters_to_remove = black_letters(best_guess, feedback);
     new_alphabet_map = Enum.reduce(letters_to_remove, alphabet_map, fn letter, acc ->
       Map.delete(acc, letter);
     end)
@@ -59,6 +51,14 @@ defmodule GameLoop do
   end
 
   def prune_possible_word_list(best_guess, feedback, possible_word_list) do
+    letters_to_remove = black_letters(best_guess, feedback);
+    new_word_list = Enum.reject(possible_word_list, fn word -> 
+      String.contains?(word, letters_to_remove)
+    end)
+    new_word_list
+  end
+
+  def black_letters(best_guess, feedback) do
     black_positions = String.graphemes(feedback) |> Enum.with_index(0) |> Enum.filter(fn {k, v} -> 
       if k == "B" do
         v
@@ -68,9 +68,5 @@ defmodule GameLoop do
     letters_to_remove = Enum.map(black_positions, fn {_k, v} ->
       Enum.at(letters_best_guess, v)
     end)
-    new_word_list = Enum.reject(possible_word_list, fn word -> 
-      String.contains?(word, letters_to_remove)
-    end)
-    new_word_list
   end
 end
